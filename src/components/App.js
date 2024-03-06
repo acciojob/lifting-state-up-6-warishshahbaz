937d9883bd3250c166a8054c3c3a149d2b7fc04a
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-03-06 19:06:01 +0530

    implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import ChildCom from "./ChildCom";
 
 const App = () => {
+  const [todos, setTodos] = useState(Data);
   return (
     <div>
-        {/* Do not remove the main div */}
+      <ChildCom todos={todos} setTodos={setTodos} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+let Data = [
+  {
+    id: 1,
+    title: "Learn React",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Build aReact app",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Deploye the React app",
+    completed: false,
+  },
+];
+export default App;
---
+++ src/components/ChildCom.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function ChildCom({ setTodos, todos }) {
+  const handliClicked = (item) => {
+    let res = todos.map((val) => {
+      if (val.id === item.id) {
+        return {
+          ...val,
+          completed: true,
+        };
+      }
+      return val;
+    });
+    setTodos(res);
+  };
+  return (
+    <div>
+      {todos.map((val, i) => {
+        return (
+          <li key={`${i}-${val}`}>
+            {val?.title ?? ""}{" "}
+            {!val?.completed && (
+              <button onClick={() => handliClicked(val)}>Complted</button>
+            )}{" "}
+          </li>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ChildCom;
